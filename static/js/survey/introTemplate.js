7289e031f38f0c6e25248b343ead51435ff18d05
jaehoonkimm <jaehoonkim.dev@gmail.com>
2022-05-13 16:32:29 +0900

    mod typo or new descriptions

--- static/js/survey/introTemplate.js
+++ static/js/survey/introTemplate.js
@@ -4,7 +4,7 @@ const guideMessage = [
     information and ideas they agree with more often.<br /><br />After the game, we hope you know more about the echo-chamber effect.</p>`,
   `<p class="explain-p">Before playing a game,<br>we want to know about you a little bit more.</p>`,
   `<p class="explain-p">Great! One more thing before we start the game.<br><br>We will give you one news headline and five related tweets,
-    and ask you to evaluate their reliability.<br><br>And this will be done four times.</p>`,
+    and ask you to evaluate their reliability.<br><br>And this will be done six times.</p>`,
   `<p class="explain-p">Now, time to play the game!</p>`,
   `<p class="explain-p">In this game, you will be playing as <span>@Johndoe</span>.<img class="profile-img" src="../static/images/johndoe.png" /><br /><br />
         <span>@Johndoe</span> is a member of an SNS community, where community members share ideas about various societal issues.<br /><br />
@@ -22,8 +22,8 @@ const guideMessage = [
         <p class="explain-p">Depending on your performance, you will get a badge with a different rank. Higher rank means more points!</p>
         <img src="../static/images/survey/rank.jpg" class="rank-img"/>
     </div>`,
-  `<p class="explain-p">So, where do you begin?<br><br>A total of eight tweets will be presented, and you will be asked to select 
-    <span class='highlight'>five</span> of them that either strongly (or weakly) support or criticize a community member’s opinion.</p>`,
+  `<p class="explain-p">So, where do you begin?<br><br>A total of six tweets will be presented, and you will be asked to select 
+    <span class='highlight'>three</span> of them that either strongly (or weakly) support a community member’s opinion.</p>`,
   `<p class="explain-p">Okay!<br><br>Let’s start the game.</p>`,
 ];
 
