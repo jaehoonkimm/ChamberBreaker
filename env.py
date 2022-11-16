b55b9c0d10766bf4e029f0af4411f603f73e2013
Jaehoon Kim <jaehoonkim.dev@gmail.com>
2022-11-16 21:09:47 +0900

    .

---
+++ env.py
@@ -0,0 +1,7 @@
+secretKey = '-42$3edzc)bz=7fznz8yedk-#6_)!rjpv+2i^9(1+)5cvn$$=^'
+allowedHosts = ['127.0.0.1', 'localhost', '3.34.131.150',
+                'ec2-3-34-131-150.ap-northeast-2.compute.amazonaws.com']
+adminID = "ajouHCI"
+adminPW = "ajouHCI@2020!"
+
+# allowedHosts = ['127.0.0.1','localhost','ec2-54-180-79-116.ap-northeast-2.compute.amazonaws.com','ap-northeast-2.compute.amazonaws.com','54.180.79.116']
