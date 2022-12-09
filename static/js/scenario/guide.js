397f1f139afe27f8490e38b01f5b616680269902
Jaehoon Kim <jaehoonkim.dev@gmail.com>
2022-12-10 01:56:54 +0900

    fix bug(send back numbers in news graph page) and fix typo

--- static/js/scenario/guide.js
+++ static/js/scenario/guide.js
@@ -1,6 +1,6 @@
 const message = {
   selectTweetGuide1: `Here are six recommended tweets.<br>Please select a tweet.`,
-  selectTweetGuide2: `Let’s share the last strong tweet.`,
+  selectTweetGuide2: `Let’s share the strongest tweet.`,
 };
 
 export default message;
